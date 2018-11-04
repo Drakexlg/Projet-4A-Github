e2fc4ec4dccaf5e6793a9e9037f88f87f525cc66
Guillaume <g.levavasseur2@laposte.net>
2018-11-04 22:05:25 +0100

    Update Superposition.cpp

---
+++ Superposition.cpp
@@ -0,0 +1,33 @@
+#include<opencv2/opencv.hpp>
+#include<iostream>
+#include<conio.h>           // may have to modify this line if not using Windows
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+int main() {
+	IplImage *imgOriginal;
+	imgOriginal = cvLoadImage("plane.png");          // open image
+	IplImage *fond;
+	fond = cvLoadImage("background.jpg");
+	IplImage *final;
+	// show windows
+	cvShowImage("GeckoGeek Image PNG & Transparence", imgOriginal);
+	IplImage *mask;
+	mask = cvCreateImage(cvGetSize(imgOriginal), 8, 1);
+	//detection du bleu et creation du mask en noir et blanc
+	cvInRangeS(imgOriginal, cvScalar(125.0, 0.0, 0.0), cvScalar(255.0 + 1.0, 130.0, 130.0), mask);
+
+	cvNot(mask, mask);
+
+	//cvShowImage("mask", mask);
+
+	cvSetImageROI(fond, cvRect(300, 30, imgOriginal->width, imgOriginal->height));
+	cvCopy(imgOriginal, fond, mask);
+	cvResetImageROI(fond);
+
+
+	cvShowImage("all", fond);
+
+	cv::waitKey(0);                 // hold windows open until user presses a key
+
+	return(0);
+}
