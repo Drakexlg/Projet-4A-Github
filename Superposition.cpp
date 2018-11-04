e16053e69825f7fd1298daa04f7e87677505d217
Guillaume <g.levavasseur2@laposte.net>
2018-11-04 23:07:20 +0100

    Update Superposition.cpp

--- Superposition.cpp
+++ Superposition.cpp
@@ -1,32 +1,58 @@
 #include<opencv2/opencv.hpp>
 #include<iostream>
 #include<conio.h>           // may have to modify this line if not using Windows
-//Attention : le fond de l'image a supperposé ne doit pas etre transparent mais plutot 
-//d'une couleur uni qui'nest pas present sur l'image !!
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
 	IplImage *imgOriginal;
 	imgOriginal = cvLoadImage("plane.png");          // open image
+	IplImage *cache;
+	cache = cvLoadImage("Cache.png");   //Obstacle du premier plan
 	IplImage *fond;
-	fond = cvLoadImage("background.jpg");
-	IplImage *final;
-	// show windows
-	cvShowImage("GeckoGeek Image PNG & Transparence", imgOriginal);
-	IplImage *mask;
-	mask = cvCreateImage(cvGetSize(imgOriginal), 8, 1);
-	//detection du bleu et creation du mask en noir et blanc
-	cvInRangeS(imgOriginal, cvScalar(125.0, 0.0, 0.0), cvScalar(255.0 + 1.0, 130.0, 130.0), mask);
+	fond = cvLoadImage("background.jpg"); 
+	
+	
+	//Creation des masks
+		//Mask objet a slider
+	IplImage *maskav;
+		maskav = cvCreateImage(cvGetSize(imgOriginal), 8, 1);
+		//detection du bleu et creation du mask en noir et blanc
+		cvInRangeS(imgOriginal, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskav); //Vert  BGR!!
+		cvNot(maskav, maskav);	
+		
+		//Mask objet de devant
+		IplImage *mask;
+		mask = cvCreateImage(cvGetSize(cache), 8, 1);
+		//detection du bleu et creation du mask en noir et blanc
+		cvInRangeS(cache, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), mask);
+		cvNot(mask, mask);
+	//Declaration rendu final
+	IplImage *Rendu;
+	Rendu = cvLoadImage("background.jpg");
+	int i;
+	//Pour faire changer la valeur de i : rester appuyer sur une touche 
+	for (i = 0; i < 1000; i++) {
+		
+		//Reinitialisation de la variable fond
+		cvSetImageROI(Rendu, cvRect(0,0, fond->width, fond->height));
+		cvCopy(fond, Rendu);
+		cvResetImageROI(Rendu);
+		//Incrustation de l'image a slider
+		cvSetImageROI(Rendu, cvRect(300+i, 30+i, imgOriginal->width, imgOriginal->height));
+		cvCopy(imgOriginal, Rendu, maskav);
+		cvResetImageROI(Rendu);
+		//Supperposition de l'obstacle visuel
+		cvSetImageROI(Rendu, cvRect(300, 30, cache->width, cache->height));
+		cvCopy(cache, Rendu, mask);
+		cvResetImageROI(Rendu);
+		//Affichage
+		cvShowImage("all", Rendu);
+		cvWaitKey(0);
+	}
 
-	cvNot(mask, mask);
+	
 
-	//cvShowImage("mask", mask);
-
-	cvSetImageROI(fond, cvRect(300, 30, imgOriginal->width, imgOriginal->height));
-	cvCopy(imgOriginal, fond, mask);
-	cvResetImageROI(fond);
-
-
-	cvShowImage("all", fond);
+	
 
 	cv::waitKey(0);                 // hold windows open until user presses a key
 
