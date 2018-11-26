0ced4b5ae266e621a614c57510e5c7f9abb72f0e
Mathrandyr <mathieu.minie@gmail.com>
2018-11-26 16:50:18 +0100

    Modification de superposition.cpp
    modification :
      -suppression de la partie avion...
      -ajout de la partie avec les vraies textures du jeu

--- Superposition.cpp
+++ Superposition.cpp
@@ -2,57 +2,99 @@
 #include<iostream>
 #include<conio.h>           // may have to modify this line if not using Windows
 
+using namespace std;
+using namespace cv;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 int main() {
-	IplImage *imgOriginal;
-	imgOriginal = cvLoadImage("plane.png");          // open image
-	IplImage *cache;
-	cache = cvLoadImage("Cache.png");   //Obstacle du premier plan
-	IplImage *fond;
-	fond = cvLoadImage("background.jpg"); 
-	
-	
-	//Creation des masks
-		//Mask objet a slider
-	IplImage *maskav;
-		maskav = cvCreateImage(cvGetSize(imgOriginal), 8, 1);
-		//detection du bleu et creation du mask en noir et blanc
-		cvInRangeS(imgOriginal, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskav); //Vert  BGR!!
-		cvNot(maskav, maskav);	
-		
-		//Mask objet de devant
-		IplImage *mask;
-		mask = cvCreateImage(cvGetSize(cache), 8, 1);
-		//detection du bleu et creation du mask en noir et blanc
-		cvInRangeS(cache, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), mask);
-		cvNot(mask, mask);
+	IplImage *imgBgnd1;
+	imgBgnd1 = cvLoadImage("bgnd1.png");          // open image
+	IplImage *imgBgnd2;
+	imgBgnd2 = cvLoadImage("bgnd21.png");   //Obstacle du premier plan
+	IplImage *imgBgnd3;
+	imgBgnd3 = cvLoadImage("bgnd31.png");
+	IplImage *imgBgnd4;
+	imgBgnd4 = cvLoadImage("bgnd41.png");          // open image
+	IplImage *imgBgnd5;
+	imgBgnd5 = cvLoadImage("bgnd51.png");   //Obstacle du premier plan
+	IplImage *imgBgnd6;
+	imgBgnd6 = cvLoadImage("bgnd61.png");
+
+	//Masques :
+	IplImage *maskbgnd2;
+	maskbgnd2 = cvCreateImage(cvGetSize(imgBgnd2), 8, 1);
+	cvInRangeS(imgBgnd2, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskbgnd2);//Vert  BGR!!
+	cvNot(maskbgnd2, maskbgnd2);
+	cvShowImage("maskbgnd2", maskbgnd2);
+
+	IplImage *maskbgnd3;
+	maskbgnd3 = cvCreateImage(cvGetSize(imgBgnd3), 8, 1);
+	cvInRangeS(imgBgnd3, cvScalar(0.0, 255.0, 60.0), cvScalar(0.0, 255.0, 90.0), maskbgnd3);
+	cvNot(maskbgnd3, maskbgnd3);
+	cvShowImage("maskbgnd3", maskbgnd3);
+
+	IplImage *maskbgnd4;
+	maskbgnd4 = cvCreateImage(cvGetSize(imgBgnd4), 8, 1);
+	cvInRangeS(imgBgnd4, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskbgnd4);
+	cvNot(maskbgnd4, maskbgnd4);
+	cvShowImage("maskbgnd4", maskbgnd4);
+
+	IplImage *maskbgnd5;
+	maskbgnd5 = cvCreateImage(cvGetSize(imgBgnd5), 8, 1);
+	cvInRangeS(imgBgnd5, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskbgnd5);
+	cvNot(maskbgnd5, maskbgnd5);
+	cvShowImage("maskbgnd5", maskbgnd5);
+
+	IplImage *maskbgnd6;
+	maskbgnd6 = cvCreateImage(cvGetSize(imgBgnd6), 8, 1);
+	cvInRangeS(imgBgnd6, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskbgnd6);
+	cvNot(maskbgnd6, maskbgnd6);
+	cvShowImage("maskbgnd6", maskbgnd6);
+
 	//Declaration rendu final
 	IplImage *Rendu;
-	Rendu = cvLoadImage("background.jpg");
-	int i;
+	Rendu = cvLoadImage("bgnd1.png");
+	
 	//Pour faire changer la valeur de i : rester appuyer sur une touche 
-	for (i = 0; i < 1000; i++) {
-		
+	
+
 		//Reinitialisation de la variable fond
-		cvSetImageROI(Rendu, cvRect(0,0, fond->width, fond->height));
-		cvCopy(fond, Rendu);
-		cvResetImageROI(Rendu);
+		cvSetImageROI(Rendu, cvRect(0, 0, Rendu->width, Rendu->height));
 		//Incrustation de l'image a slider
-		cvSetImageROI(Rendu, cvRect(300+i, 30+i, imgOriginal->width, imgOriginal->height));
-		cvCopy(imgOriginal, Rendu, maskav);
+		cvSetImageROI(Rendu, cvRect(0, 0, imgBgnd6->width, imgBgnd6->height));
+		cvCopy(imgBgnd6, Rendu, maskbgnd6);
 		cvResetImageROI(Rendu);
-		//Supperposition de l'obstacle visuel
-		cvSetImageROI(Rendu, cvRect(300, 30, cache->width, cache->height));
-		cvCopy(cache, Rendu, mask);
+
+		cvSetImageROI(Rendu, cvRect(0, 0, imgBgnd5->width, imgBgnd5->height));
+		cvCopy(imgBgnd5, Rendu, maskbgnd5);
 		cvResetImageROI(Rendu);
+
+		cvSetImageROI(Rendu, cvRect(0, 0, imgBgnd4->width, imgBgnd4->height));
+		cvCopy(imgBgnd4, Rendu, maskbgnd4);
+		cvResetImageROI(Rendu);
+
+		cvSetImageROI(Rendu, cvRect(0, 0, imgBgnd3->width, imgBgnd3->height));
+		cvCopy(imgBgnd3, Rendu, maskbgnd3);
+		cvResetImageROI(Rendu);
+
+		cvSetImageROI(Rendu, cvRect(0,0, imgBgnd2->width, imgBgnd2->height));
+		cvCopy(imgBgnd2, Rendu, maskbgnd2);
+		cvResetImageROI(Rendu);
+
+		IplImage *maskbgnd7;
+		maskbgnd7 = cvCreateImage(cvGetSize(Rendu), 8, 1);
+		cvInRangeS(Rendu, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskbgnd7);
+		cvNot(maskbgnd7, maskbgnd7);
+		cvShowImage("maskbgnd7", maskbgnd7);
+
 		//Affichage
 		cvShowImage("all", Rendu);
-		cvWaitKey(0);
-	}
+		cout << Rendu;
 
-	
+		Mat mtx = cv::cvarrToMat(Rendu);
+		
+		cout << "M = " << endl << " " << mtx << endl << endl;
 
-	
+
 
 	cv::waitKey(0);                 // hold windows open until user presses a key
 
