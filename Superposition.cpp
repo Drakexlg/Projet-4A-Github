c2b1f85ae971dc6b3a99422bf657d42d71505d92
Mathrandyr <mathieu.minie@gmail.com>
2018-11-30 20:10:32 +0100

    Ajout des commentaires dans le code

--- Superposition.cpp
+++ Superposition.cpp
@@ -23,11 +23,20 @@ int main() {
 	Arguments de cvCreateImage : (size,color_depth,channels)
 	Size : largeur et longueur de l'image (en nombre de pixels)
 	Color_depth : nombre de valeurs que peut prendre une couleur ex: pour 8 bits le rouge,le vert et le bleu seront codés sur 256 valeurs
-	channels : "http://images.math.cnrs.fr/le-traitement-numerique-des-images.html*   	*/
-	
-	maskbgnd2 = cvCreateImage(cvGetSize(imgBgnd2), 8, 1);  //transformation du masque
-	cvInRangeS(imgBgnd2, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskbgnd2);//Vert  BGR!!
-	cvNot(maskbgnd2, maskbgnd2);
+	channels : RGB = 3 channels, or le masque est une image en échelle de gris donc un seul canal est nécessaire  donc canal = 1
+	"http://images.math.cnrs.fr/le-traitement-numerique-des-images.html*   	*/
+	maskbgnd2 = cvCreateImage(cvGetSize(imgBgnd2), 8, 1);  //créé un "receptacle" de la taille de l'image bgnd2, de couleur 8 bits en échelle de gris
+
+	/*cvInRangeS : créé un masque à partir d'une image
+	arguments de cvInRangeS : (InputArray source, InputArray lowerb, InputArray upperb, OutputArray destination)
+	InputArray Source : image en couleur qui va être échantillonnée pour créer le masque
+	InputArray lowerb : écriture scalaire de la couleur RGB "basse"  ATTENTION la syntaxe n'est pas RGB mais BGR dans opencv
+  InputArray lowerb : écriture scalaire de la couleur RGB "haute"  ATTENTION la syntaxe n'est pas RGB mais BGR dans opencv
+	toutes les couleurs comprises dans l'intervalle [lowerb,upperb] seront transcrites en blanc dans le masque, les autres en noir
+	OutputArray destination : objet de sortie qui comprendra le masque
+	*/
+	cvInRangeS(imgBgnd2, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskbgnd2);//détection du fond vert et création du masque à partir de celui-ci
+	cvNot(maskbgnd2, maskbgnd2); //inversion du blanc et du noir du masque
 
 
 	IplImage *maskbgnd3;
@@ -52,6 +61,7 @@ int main() {
 	maskbgnd6 = cvCreateImage(cvGetSize(imgBgnd6), 8, 1);
 	cvInRangeS(imgBgnd6, cvScalar(0.0, 255.0, 76.0), cvScalar(0.0, 255.0, 76.0), maskbgnd6);
 	cvNot(maskbgnd6, maskbgnd6);
+
 	/* AFFICHAGE DES MASQUES
 	cvShowImage("maskbgnd2", maskbgnd2);
 	cvShowImage("maskbgnd3", maskbgnd3);
@@ -59,13 +69,21 @@ int main() {
 	cvShowImage("maskbgnd5", maskbgnd5);
 	cvShowImage("maskbgnd6", maskbgnd6);*/
 
-		//Reinitialisation de la variable fond
-		cvSetImageROI(Rendu, cvRect(0, 0, Rendu->width, Rendu->height));
-		//Incrustation de l'image a slider
-		cvSetImageROI(Rendu, cvRect(0, 0, imgBgnd6->width, imgBgnd6->height));
-		cvCopy(imgBgnd6, Rendu, maskbgnd6);
-		cvResetImageROI(Rendu);
+		/*cvSetImageROI : definit la région d'intêret d'une image (ROI = Region Of Interest): permet de ne se concentrer que sur une certaine partie de l'image
+		arguments de cvSetImageROI : (IplImage image, cvRect rect)
+		IplImage image : représente l'image dont on veut changer le cvSetImageROI
+		cvRect rect : Rectangle composé d'un point d'origine, d'une longueur et d'une largeur
+		*/
+		cvSetImageROI(Rendu, cvRect(0, 0, Rendu->width, Rendu->height)); // on définit le ROI du rendu final sur toute l'image (on souhaite voir toute l'image)
 
+		//Incrustation de l'image a slider
+		cvSetImageROI(Rendu, cvRect(0, 0, imgBgnd6->width, imgBgnd6->height)); //on définit le ROI du rendu final sur le ROI de l'image qu'on va copier dessus afin que si
+		//il y a un écart de résolution entre l'image à copier et le rendu, le programme ne plante pas
+		cvCopy(imgBgnd6, Rendu, maskbgnd6); //on copie l'image imgBgnd6 sur le rendu final en utilisant le masque maskbgnd6 créé précédemment pour ne pas copier le vert dessus
+		cvResetImageROI(Rendu); //on reset le ROI du rendu final
+
+		//on continue de superposer les images sur le rendu final en faisant attention à copier les calques d'arrière plan en premier et en remontant de cette manière
+		//vers le premier plan
 		cvSetImageROI(Rendu, cvRect(0, 0, imgBgnd5->width, imgBgnd5->height));
 		cvCopy(imgBgnd5, Rendu, maskbgnd5);
 		cvResetImageROI(Rendu);
@@ -82,10 +100,10 @@ int main() {
 		cvCopy(imgBgnd2, Rendu, maskbgnd2);
 		cvResetImageROI(Rendu);
 
-		//Affichage
+		//Affichage du rendu final
 		cvShowImage("all", Rendu);
 
-	cv::waitKey(0);                 // hold windows open until user presses a key
+	cv::waitKey(0);     //l'appui sur n'importe quelle touche fera interrompre le programme
 
 	return(0);
 }
